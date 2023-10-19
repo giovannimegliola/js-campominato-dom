302468df072c61830fd5b4cd16eab75a1c18bc5c
giovannimegliola <giovanni.megliola@hotmail.it>
2023-10-19 16:02:03 +0200

    modified numCell based on selection

--- js/script.js
+++ js/script.js
@@ -1,26 +1,38 @@
 const btn = document.querySelector('button');
+const difficultySelect = document.getElementById('levels');
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function() {
+  let numCell;
 
-  const numCell = 100;   //celle da generare
+  // Leggi il livello di difficoltà selezionato dalla select
+  const selectedDifficulty = difficultySelect.value;
 
-  const playground = document.getElementById('playground');
-
-  playground.innerHTML = ''; //reset
-
-  for (let i = 1; i <= numCell; i++){    //ciclo sulla griglia per stampare le celle
-    let cell = drawCell(i);           // genero le celle
-    playground.append(cell);        //aggiungo la cella alla griglia
+  // Imposta il numero di celle in base al livello di difficoltà
+  if (selectedDifficulty === '1') {
+    numCell = 100;
+  } else if (selectedDifficulty === '2') {
+    numCell = 81;
+  } else if (selectedDifficulty === '3') {
+    numCell = 49;
   }
 
-})
+  const playground = document.getElementById('playground');
+  playground.innerHTML = ''; //reset
+
+  for (let i = 1; i <= numCell; i++) {
+    let cell = drawCell(i,numCell);
+    playground.append(cell);
+  }
+});
 
 // funzione per disegnare le celle nella griglia 10x10
-
-function drawCell(cellNumber){
+function drawCell(cellNumber,numCell) {
+  const cellWidth = Math.sqrt(numCell);
   const cell = document.createElement('div');
   cell.classList.add('cell');
-  cell.addEventListener('click', function(){      //cambio colore alla cella quando ci clicco
+  cell.style.width = `calc(100% / ${cellWidth})`;
+  cell.style.height = cell.style.width;
+  cell.addEventListener('click', function() {
     cell.classList.add('active');
     console.log('hai cliccato la cella:' + cellNumber);
   });
